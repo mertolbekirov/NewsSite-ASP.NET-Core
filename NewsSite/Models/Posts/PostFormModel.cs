af6bcb3eadfcd5bbd3cf6562b3827774c9fe3ed8
mertolbekirov <mertol2002@gmail.com>
2022-07-30 21:53:39 +0300

    tinymce integration

---
+++ NewsSite/Models/Posts/PostFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsSite.Models.Posts
+{
+    public class PostFormModel
+    {
+        public string VideoUrl { get; set; }
+
+        public byte[] InPostImageData { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+    }
+}
